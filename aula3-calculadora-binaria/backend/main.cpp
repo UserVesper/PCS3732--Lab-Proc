3b11f59e7472a21cb0acafee73b22ddc45337a8b
RodrigoGalvezLima <rodrigogalvezlima@usp.br>
2026-05-26 21:21:29 -0300

    feat: adding server features

--- aula3-calculadora-binaria/backend/main.cpp
+++ aula3-calculadora-binaria/backend/main.cpp
@@ -1,19 +1,32 @@
-const int LEDS[] = {12,13,14,15}
+
+#include <WebServer.h>
+
+const int LEDS[] = {12,13,14,15};
+
+WebServer server(80);
 
 void handleCalc(){
-    String paramA = "0011";
-    String paramB = "0101";
-    int valueA = strtol(paramA.c_str(), NULL, 2);
-    int valueB = strtol(paramB.c_str(), NULL, 2);
+    if(server.hasArg("a") && server.hasArg("b") && server.hasArg("op")){
 
-    int result = valueA + valueB;
-    result = result & 0x0F;
-    
-    digitalWrite(LEDS[0], result & 0x01);
-    digitalWrite(LEDS[1], (result >> 1) & 0x01); 
-    digitalWrite(LEDS[2], (result >> 2) & 0x01); 
-    digitalWrite(LEDS[3], (result >> 3) & 0x01);
+        String paramA = server.arg("a");
+        String paramB = server.arg("b");
+        String op = server.arg("op");
 
+        int valueA = strtol(paramA.c_str(), NULL, 2);
+        int valueB = strtol(paramB.c_str(), NULL, 2);
+
+        int result = (op == "add") ? (valueA + valueB) : (valueA - valueB);
+        result = result & 0x0F;
+        
+        digitalWrite(LEDS[0], result & 0x01);
+        digitalWrite(LEDS[1], (result >> 1) & 0x01); 
+        digitalWrite(LEDS[2], (result >> 2) & 0x01); 
+        digitalWrite(LEDS[3], (result >> 3) & 0x01);
+        
+        server.send(200, "text/plain", "OK");
+    } else {
+        server.send(400, "text/plain", "Faltam parâmetros");
+    }
 }
 
 void setup() {
@@ -25,8 +38,11 @@ digitalWrite(LEDS[0], LOW);
 digitalWrite(LEDS[1], LOW);
 digitalWrite(LEDS[2], LOW);
 digitalWrite(LEDS[3], LOW);
+
+server.on("/calc", handleCalc);
+server.begin();
 }
 
 void loop() {
-
+server.handleClient();
 }
